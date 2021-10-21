825a5659bc304a96927c8aa43b15153d36972e4b
Michael Moser <moser.michael@gmail.com>
2021-10-21 04:10:35 +0300

    fix requirements.txt

---
+++ makerequirements.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ex
+
+python3 -m venv req-venv
+source req-venv/bin/activate
+
+pip3 install PyGithub
+pip3 install websocket-client
+
+pip3 freeze >requirements.txt
+
+deactivate req-venv
+rm -rf req-venv
+
+echo "** got requirements.txt **"
